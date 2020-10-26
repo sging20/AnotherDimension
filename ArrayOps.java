dd6d92d284e4185d99fccfcc0ccb8866736c4bc5
Sean Ging <sging20@stuy.edu>
2020-10-25 21:14:37 -0400

    I have found progress

--- ArrayOps.java
+++ ArrayOps.java
@@ -22,3 +22,14 @@ public static  int largest(int[]arr)
   }
   return max;
 } //call function in the public static void main(String[] args)
+
+//4c. sumRows() takes a 2D array of int, and returns a 1D array of int with each element being the sum of the individual rows of the 2D array.
+public static int[] sumRows(int[][] matrix)
+{
+  int[]out = new int [matrix.length]; //new array
+  for(int i=0; i< matrix.length; i++) //going through the columns of the array
+  {
+    out[i]= sum(matrix[i]);
+  }
+  return out;  // might have to call this function in the main
+}
