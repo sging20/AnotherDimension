ad4a60929177904c8bf8dd7788b14399d08f8808
Sean Ging <sging20@stuy.edu>
2020-10-26 08:08:16 -0400

    Made fixes

--- ArrayOps.java
+++ ArrayOps.java
@@ -28,7 +28,7 @@ public static  int largest(int[]arr)
 //4c. sumRows() takes a 2D array of int, and returns a 1D array of int with each element being the sum of the individual rows of the 2D array.
 public static int[] sumRows(int[][] matrix)
 {
-  int[]out = new int [matrix.length]; //new array
+  int[] out = new int [matrix.length]; //new array
   for(int i=0; i< matrix.length; i++) //going through the columns of the array
   {
     out[i]= sum(matrix[i]);
@@ -39,7 +39,7 @@ public static int[] sumRows(int[][] matrix)
 //4d. largestInRows() takes a nonempty 2D array of int, the inner arrays cannot be empty either, and returns a 1D array of int with each element being the largest element of the individual rows  of the 2D array.
 public static  int[] largestInRows(int[][] matrix)
 {
-int[]out = new int[matrix.length]; //new array
+int[] out = new int[matrix.length]; //new array
 for(int i=0; i< matrix.length; i++)
 {
 out[i] = largest(matrix[i]);
@@ -61,25 +61,22 @@ return sum;
 //a 1D array of int with each element being the sum of the individual columns of the 2D array.
 public static int[] sumCols(int[][] matrix)
 {
-  int[]switch = int[matrix.length];
+  int[] out = new int[matrix[0].length];
   for(int i=0; i< matrix.length; i++)
   {
-    for(int j=0; j, matrix[1].length; j++)
+    for(int j=0; j< matrix[0].length; j++)
     {
-      switch[j][i] = matrix[i][j];
-    }
-  return sumRows(switch);
-}
+      output[col] += matrix[row][col];
+return sumRows;
 }
 
 //5b) isRowMagic() takes a rectangular 2d array and returns true when each row has the same sum, and false otherwise.
-public static boolean isRowMagic(int[][] matrix)
-{
+public static boolean isRowMagic(int[][] matrix) {
   boolean isMagic = true;
-  int[] sumofRows = sumRows(matrix);
-  for(int i=0; i<matrix.length-1; i++)
+  int[] sum = sumRows(matrix);
+  for(int i=0; i<matrix.length -1; i++)
   {
-    if([i] != [i+1])
+    if(sum[i] != sum[i+1])
     {
       isMagic = false; //as the law says, Innocent until proven GUILTY!
     }
@@ -92,7 +89,7 @@ boolean isMagic = true;
 int[] sumsofCols = sumCols(matrix);
 for(int i=0; i<matrix[1].length-1; i++)
 {
-  if([i] != [i+1])
+  if(sumofCols[i] != sumofCols[i+1])
   {
     isMagic = false; //as the law says, Innocent until proven GUILTY!
   }
@@ -104,14 +101,13 @@ return isMagic;
 //5d)  isLocationMagic()  takes a rectangular 2d array and returns true when the row and column specified have the same sum.
 public static boolean isLocationMagic(int[][] matrix, int row, int col)
 {
-boolean isMagic = true;
-int[] sumofRows = sumRows(matrix);
-int[] sumofCols= sumCols(matrix);
-  if(sumofRows[row] != sumofCols[col])
+int[] Rows = sumRows(matrix);
+int[] Cols= sumCols(matrix);
+  if(Rows[row] != Cols[col])
   {
-    isMagic= false;
+    return false;
   }
-  return isMagic;
+  else return true;
 }
 
 }
