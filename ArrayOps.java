b8347455782ff6090b3f340972bd94593ae4cb8c
Sean Ging <sging20@stuy.edu>
2020-10-25 21:51:58 -0400

    Did isRows, cuz accidentally did isColsMagic first, oops

--- ArrayOps.java
+++ ArrayOps.java
@@ -72,10 +72,24 @@ int[]switch = int[matrix.length];
 }
 
 //5b) isRowMagic() takes a rectangular 2d array and returns true when each row has the same sum, and false otherwise.
-public static boolean isRowMagic(int[][] matrix) {
+public static boolean isRowMagic(int[][] matrix)
+{
+  boolean isMagic = true;
+  int[] sumofRows = sumRows(matrix);
+  for(int i=0; i<matrix.length-1; i++)
+  {
+    if([i] != [i+1])
+    {
+      isMagic = false; //as the law says, Innocent until proven GUILTY!
+    }
+  }
+  return isMagic;
+}
+//5c) isColMagic()  takes a rectangular 2d array and  returns true when each column has the same sum, and false otherwise.
+public static boolean isColumnMagic(int[][] matrix) {
 boolean isMagic = true;
 int[] sumsofCols = sumCols(matrix);
-for(int i=0; i<matrix.length-1; i++)
+for(int i=0; i<matrix[1].length-1; i++)
 {
   if([i] != [i+1])
   {
