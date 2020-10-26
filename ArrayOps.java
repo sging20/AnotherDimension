64d1068c834ae8049c91f6b4ceca206297caff08
Sean Ging <sging20@stuy.edu>
2020-10-25 20:58:09 -0400

    I tested this program and 4a works

--- ArrayOps.java
+++ ArrayOps.java
@@ -8,17 +8,3 @@ public int[] sum(int[] arr)
   }
   return sum; // might have to call this function in the main
 }
-
-//4b). largest() takes an array of int, and returns the largest element of the array.
-public static  int largest(int[]arr)
-{
-  int max = arr[0];
-  for(int i=0; i<arr.length; i++)
-  {
-    if(nums[i] > max)
-    {
-      max = arr[i];
-    }
-  }
-  return max;
-} //call function in the public static void main(String[] args)
